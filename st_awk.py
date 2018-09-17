26db73cf23cad5b6ca378c142b13b46702b7984c
Alexandr Mitkalev <iroota@gmail.com>
2018-09-17 19:57:11 +0300

    Refactoring for working in Sublime text 3

--- st_awk.py
+++ st_awk.py
@@ -2,46 +2,52 @@
 import sublime, sublime_plugin
 from subprocess import Popen, PIPE, STDOUT
 
-class st_awkCommand(sublime_plugin.TextCommand):
-    def run_command(self, edit, expr, inplace):
+class awk_runCommand(sublime_plugin.TextCommand):
+    def run_(self, edit_token, args):
         """
         Actually run awk command
         """
+        expr, inplace = args.get('expr'), args.get('inplace')
+
+        if expr == None:
+            return
+
         r = sublime.Region(0, self.view.size())
         content = self.view.substr(r)
 
-        out = ''
         try:
             p = Popen(['awk', expr], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-            out, err = p.communicate(input=content)
-            if err != '':
+            out, err = map(bytes.decode, p.communicate(input=content.encode('utf8')))
+            if err:
                 sublime.error_message('Error when run awk: \n' + err)
                 return
-        except Exception as e:
+        except OSError as e:
             sublime.error_message('''Error when run command awk: %s, errno: %d.\nawk is required on $PATH''' 
                 % (e.strerror, e.errno))
             return
-        except UnicodeEncodeError as e:
-            sublime.error_message('''Error in document encode''')
+        except Exception as e:
+            sublime.error_message('''Error when run command awk: %s''' % e)
             return
 
         if inplace:
-            self.view.replace(edit, sublime.Region(0, self.view.size()), out)
+            edit = self.view.begin_edit(edit_token, self.name())
+            self.view.replace(edit, r, out)
+            self.view.end_edit(edit)
         else:
             new_view = self.view.window().new_file()
+            edit = new_view.begin_edit(edit_token, self.name())
             new_view.insert(edit, 0, out)
+            new_view.end_edit(edit)
 
-
-    def run(self, edit, **args):
+class st_awkCommand(sublime_plugin.TextCommand):
+    def run(self, *args, **kwargs):
         """
         args: 
             'inplace' : if it's 'yes', replace current view with command result 
         """
-        inplace = args['inplace'].lower() == 'yes'
+        inplace = kwargs['inplace'].lower() == 'yes'
         def onDone(expr):
-            self.run_command(edit, expr, inplace)
-
-        self.view.window().show_input_panel('Awk expression:', '', onDone, None, None)
-        
+            self.view.run_command('awk_run', {'expr': expr, 'inplace': inplace})
 
 # TODO in-line awk expr or .awk file
+        self.view.window().show_input_panel('Awk expression:', '', onDone, None, None)
